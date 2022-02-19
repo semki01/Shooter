0b6e55000533d81ca27de599d2683011661ce1bf
Semki 01 <91430213+semki01@users.noreply.github.com>
2022-02-19 13:41:36 +0200

    Update shuter.py

--- Шутер/shuter.py
+++ Шутер/shuter.py
@@ -1,10 +1,10 @@
 from pygame import*
 from random import randint
 
-#mixer.init()
-#mixer.music.load('space.ogg')
-#mixer.music.play()
-#fire_sound = mixer.Sound('fire.ogg')
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
 
 img_back = 'galaxy.jpg'
 img_hero = 'rocker.png'
@@ -41,7 +41,9 @@ class Player(GameSprite):
             self.rect.x += self.speed
 
     def fire(self):
-        pass
+        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+    
 lost = 0
 
 class Enemy(GameSprite):
@@ -54,29 +56,61 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost = lost + 1
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+
+        if self.rect.y < 0:
+            self.kill()
+
+
+
+font.init()
+font2 = font.SysFont('Arial', 36)
 
 rocket = Player('rocket.png', 310, 350, 80, 110, 10)
-ship = Enemy('ufo.png', randint(5, 500), 5, 70, 60, 4)
+ship = Enemy('ufo.png', randint(5, 500), 5, 70, 50, 4)
+
+
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy('ufo.png', randint(80, win_width - 80), -40, 80, 50, randint(1,5))
+    monsters.add(monster)
+
+bullets = sprite.Group()
 
 finish = False
 clock = time.Clock()
 run = True
 FPS = 60
 
-
-
+score = 0
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                rocket.fire()
 
     if not finish:
         window.blit(background,(0,0))
+        text = font2.render('Cчёт:' + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render('Пропущенно:' + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
         rocket.update()
         rocket.reset()
+        bullets.update()
+        bullets.draw(window)
         ship.update()
         ship.reset()
+        monsters.update()
+        monsters.draw(window)
         display.update()
 
     time.delay(50)
